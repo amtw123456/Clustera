f6b2ccd90ab8e687d5ebc23ab704b2256e5ba2c8
Ronel Dylan Joshua A. Dolor <80044625+amtw123456@users.noreply.github.com>
2024-03-27 13:22:20 +0800

    chore: changed file name

--- frontend/clustera-react-app/src/App.js
+++ frontend/clustera-react-app/src/App.js
@@ -4,8 +4,8 @@ import UploadPage from './pages/upload_page/UploadPage.js';
 import DocPage from './pages/documents_page/DocPage.js';
 import LDApage from './pages/cluster_page_lda/LDApage.js';
 import LSApage from './pages/cluster_page_lsa/LSApage.js';
-import AboutPage from './pages/about_page/About.js';
-import DocumentationPage from './pages/documentation_page/Documentation.js';
+import AboutPage from './pages/about_page/about.js';
+import DocumentationPage from './pages/documentation_page/documentations.js';
 import { AppStateProvider } from './providers/AppState.js';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
