936f7b147e3128d32105735c80bcd260d97a6bc0
Ronel Dylan Joshua A. Dolor <80044625+amtw123456@users.noreply.github.com>
2024-03-27 13:08:24 +0800

    chore: added documentation and about page

---
+++ frontend/clustera-react-app/src/pages/about_page/about.js
@@ -0,0 +1,16 @@
+// ClusteraApp.jsx
+import React, { useState } from 'react';
+import { Upload } from "./Upload";
+import { Link } from "react-router-dom";
+import NavigationBar from '../../components/navbar.js';
+
+const AboutPage = () => {
+    return (
+        <div>
+            <NavigationBar />
+            <div>About Page</div>
+        </div>
+    );
+};
+
+export default AboutPage;
