b8ebd50a192db2a01b7251ce27a5749b37df291d
Ronel Dylan Joshua A. Dolor <80044625+amtw123456@users.noreply.github.com>
2024-03-28 22:41:25 +0800

    feat: added text notification when there is no file uploaded or the text has not been tokenized

--- frontend/clustera-react-app/src/pages/documents_page/DocPage.js
+++ frontend/clustera-react-app/src/pages/documents_page/DocPage.js
@@ -125,45 +125,12 @@ function DocPage() {
               )
             )
           }</li>
-          <li><Link to="/cluster_page_lda" class="block py-2 px-5 text-black hover:bg-gray-300">Cluster Using LSA</Link></li>
-          <li><Link to="/cluster_page_lsa" class="block py-2 px-5 text-black hover:bg-gray-300">Cluster Using LDA</Link></li>
+          <li><Link to="/cluster_page_lsa" class="block py-2 px-5 text-black hover:bg-gray-300">Cluster Using LSA</Link></li>
+          <li><Link to="/cluster_page_lda" class="block py-2 px-5 text-black hover:bg-gray-300">Cluster Using LDA</Link></li>
         </ul>
       </div>
       <div class="mx-auto w-3/5">
       </div>
-
-      {/* <div class="flex items-center justify-center mb-5">
-        {
-          isLoading ? (
-            // <div>
-            //     <svg aria-hidden="true" class="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-purple-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-            //         <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
-            //         <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
-            //     </svg>
-            //     <span class="sr-only">Loading...</span>
-            // </div>
-            <div>
-              <button disabled type="button" class="w-60 bg-blue-500 mx-3 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded justify-center flex items-center">
-                <svg aria-hidden="true" role="status" class="inline w-5 h-5 me-3 text-white animate-spin fill-purple-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
-                  <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
-                </svg>
-                Loading...
-              </button>
-            </div>
-          ) : (
-            isPreProcessed ? (
-              <button className="w-60 bg-gray-500 mx-3 text-white font-bold py-2 px-4 rounded" disabled>
-                {"Text Pre-Processing"}
-              </button>
-            ) : (
-              <button className="w-60 bg-blue-500 mx-3 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => preprocessText()}>
-                {"Text Pre-Processing"}
-              </button>
-            )
-          )
-        }
-      </div> */}
 
       {/* <div class="flex items-center justify-center mb-3">
         <button
@@ -256,7 +223,11 @@ function DocPage() {
                 <PDocumentsCard key={index} index={index} item={item} />
               ))
             ) : (
-              <div className="text-center text-gray-600 dark:text-gray-400 mt-4">It's empty</div>
+              <div class="flex-1">
+                <div className="text-center mt-48 text-gray-600 text-base dark:text-gray-400">Documents have not yet been tokenized!</div>
+                <div className="text-center text-gray-600 text-xl dark:text-gray-400">Please press the Text Pre-Processing Button.</div>
+                {/* <Link to="/"><div class="text-center text-blue-600 text-base underline-offset-4">Go to Upload Page</div></Link> */}
+              </div>
             )
           )
         ) :
@@ -265,7 +236,10 @@ function DocPage() {
               <UDocumentsCard key={index} index={index} item={item} />
             ))
           ) : (
-            <div className="text-center text-gray-600 dark:text-gray-400 mt-4">It's empty</div>
+            <div class="flex-1">
+              <div className="text-center mt-48 text-gray-600 text-xl dark:text-gray-400">There are no Documents loaded yet to the page please Upload a JSON or CSV file.</div>
+              <Link to="/"><div class="text-center text-blue-600 text-base underline-offset-4">Go to Upload Page</div></Link>
+            </div>
           )
         }
       </div >
