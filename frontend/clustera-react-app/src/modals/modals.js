626cddefb4f0ca46e2bc26943132b26138bb54d1
Ronel Dylan Joshua A. Dolor <80044625+amtw123456@users.noreply.github.com>
2024-03-30 00:10:19 +0800

    feat: added to documents class 'documentTopicDistribution' field

--- frontend/clustera-react-app/src/modals/modals.js
+++ frontend/clustera-react-app/src/modals/modals.js
@@ -1,10 +1,11 @@
 export class Document {
-    constructor(uDocument, pDocument, clusterId, clusterLabel, topics, documentTokens) {
-        this.uDocument = uDocument
-        this.pDocument = pDocument
+    constructor(uDocument, pDocument, clusterId, clusterLabel, topics, documentTopicDistribution, documentTokens) {
+        this.uDocument = uDocument;
+        this.pDocument = pDocument;
         this.clusterId = clusterId;
         this.clusterLabel = clusterLabel;
-        this.documentTokens = documentTokens
+        this.documentTokens = documentTokens;
+        this.documentTopicDistribution = documentTopicDistribution;
         this.topics = topics;
     }
 }
