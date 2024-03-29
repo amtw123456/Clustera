66c2bb70b968fa3fbff62daf5015965ece7b31da
Ronel Dylan Joshua A. Dolor <80044625+amtw123456@users.noreply.github.com>
2024-03-29 14:00:22 +0800

    feat: added loading when uploaded data is not yet finished loading, also fixed Document class can now be exported

--- frontend/clustera-react-app/src/modals/modals.js
+++ frontend/clustera-react-app/src/modals/modals.js
@@ -1,10 +1,9 @@
-class Document {
+export class Document {
     constructor(uDocument, pDocument, clusterLabel, topics, clusterId) {
         this.uDocument = uDocument
         this.pDocument = pDocument
         this.clusterId = clusterId;
         this.clusterLabel = clusterLabel;
         this.topics = topics;
-
     }
 }
