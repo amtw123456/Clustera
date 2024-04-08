16e4c7cc416cdd0ba83a54366a90035aa32a8cc8
Ronel Dylan Joshua A. Dolor <80044625+amtw123456@users.noreply.github.com>
2024-04-08 12:00:48 +0800

    feat: flexible datasummary page when browser is minimized data can be viewed using scrolls

--- frontend/clustera-react-app/src/components/datasummary.jsx
+++ frontend/clustera-react-app/src/components/datasummary.jsx
@@ -11,7 +11,7 @@ function DataSummarySection({ summarizedDocuments, noOfClusters, topicCoheranceG
                 {/* <div class="ml-3 font-bold italic">Document Topic Distribution:</div> */}
                 <div class="p-2 flex flex-col w-1/3 overflow-auto border-grey-300 border rounded-lg mt-3">
                     <div class="font-bold w-full h-[15px] text-center mb-5">Topics Summary</div>
-                    <div class="w-full flex flex-col">
+                    <div class="w-full flex flex-col w-[480px]">
                         {topicsGenerated.map((topics, innerIndex) => (
                             <div class="border-yellow border flex flex-col w-full h-[68px] mb-1 overflow-hidden rounded-md text-teal-400 mb-1 rounded-md bg-teal-100 border-teal-400">
                                 <div class="ml-5 flex flex-row flex-wrap italic w-[500px] overflow font-bold text-base items-center">
@@ -31,20 +31,20 @@ function DataSummarySection({ summarizedDocuments, noOfClusters, topicCoheranceG
                         ))}
                     </div>
                 </div>
-                <div class="ml-2 p-2 flex flex-col w-2/3 overflow-auto border-grey-300 border rounded-lg mt-3">
-                    <div class="flex-row flex">
-                        <div class="w-1/2 flex flex-col">
-                            <div class="font-bold w-full h-[15px] text-center mb-5">Clustered Summary</div>
-                            <div class="w-full flex flex-col">
+                <div class="ml-2 p-2 flex flex-col w-2/3 border-grey-300 border rounded-lg mt-3">
+                    <div class="flex-row flex overflow-hidden">
+                        <div class="w-1/2 flex flex-col px-1 overflow-auto">
+                            <div class="font-bold h-[15px] text-center mb-5">Clustered Summary</div>
+                            <div class="flex flex-col">
                                 {Array.from(Array(noOfClusters), (item, index) => (
-                                    <div class="border-yellow border flex flex-col w-full h-[68px] mb-1 overflow-hidden rounded-md text-teal-400 mb-1 rounded-md bg-teal-100 border-teal-400">
-                                        <div class="ml-5 flex flex-row flex-wrap italic w-[500px] overflow font-bold text-base items-center">
+                                    <div class="border-yellow border flex flex-col w-[480px] h-[68px] mb-1 rounded-md text-teal-400 rounded-md bg-teal-100 border-teal-400">
+                                        <div class="ml-5 flex flex-row flex-wrap italic font-bold text-base items-center">
                                             <div class="flex text-base flex ml-[3px] px-1 ">Cluster {index} Number of documents:</div>
-                                            <div class="overflow-hidden flex flex-wrap">
+                                            <div class="flex flex-wrap">
                                                 <div class="flex ml-[3px] px-1 border font-bold text-base text-purple-400 m-1 rounded-md bg-purple-100 border-purple-400" >{clustersGenerated[index].length} Documents</div>
                                             </div>
                                         </div>
-                                        <div class="ml-5 flex flex-row flex-wrap italic w-[500px] overflow font-bold text-base items-center">
+                                        <div class="ml-5 flex flex-row flex-wrap italic font-bold text-base items-center">
                                             <div class="flex text-base flex ml-[3px] px-1 ">Cluster Category Label: NaN</div>
                                             {/* <div class='flex text-base italic rounded-md bg-green-100 border-green-400 text-green-400 px-1 border rounded-md'>{topicCoheranceGenerated[innerIndex]}</div> */}
                                         </div>
@@ -53,7 +53,7 @@ function DataSummarySection({ summarizedDocuments, noOfClusters, topicCoheranceG
                                 ))}
                             </div>
                         </div>
-                        <div class="flex flex-col w-1/2">
+                        <div class="flex flex-col w-1/2 overflow-x-auto overflow-y-hidden">
                             <div class="font-bold w-full h-[15px] text-center">Clustered Data Donut Chart</div>
                             <div class="w-full flex flex-col">
                                 <DonutChart data={data} width={500} height={400} />
