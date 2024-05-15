96efa8eb482b6e9b1d7a245579d2921bcd5b7364
Ronel Dylan Joshua A. Dolor <80044625+amtw123456@users.noreply.github.com>
2024-05-15 14:40:08 +0800

    feat:modified documentation page

--- frontend/clustera-react-app/src/pages/documentation_page/documentations.jsx
+++ frontend/clustera-react-app/src/pages/documentation_page/documentations.jsx
@@ -5,10 +5,19 @@ import NavigationBar from '../../components/navbar.jsx';
 
 const DocumentationPage = () => {
     return (
-        <div>
+        <>
             <NavigationBar />
-            <div>Documentation Page</div>
-        </div>
+            <div className='flex flex-col mt-6 items-center'>
+                <div className='bg-red-100 w-4/5 h-[500px]'>Clustera is </div>
+                <div className='bg-red-100 w-4/5 h-[500px]'>Clustera is </div>
+                <div className='bg-red-100 w-4/5 h-[500px]'>Clustera is </div>
+                <div className='bg-red-100 w-4/5 h-[500px]'>Clustera is </div>
+                <div className='bg-red-100 w-4/5 h-[500px]'>Clustera is </div>
+                <div className='bg-red-100 w-4/5 h-[500px]'>Clustera is </div>
+                <div className='bg-red-100 w-4/5 h-[500px]'>Clustera is </div>
+
+            </div>
+        </>
     );
 };
 
